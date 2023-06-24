29d83f2dd4ef50531295c87a7567a0ac8f80b730
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-06-24 20:03:09 +0000

    Basic WebConnection API structure.

--- src/deno/net.mjs
+++ src/deno/net.mjs
@@ -3,7 +3,134 @@
 
 "use strict";
 
+// Raw Connections
+let RawConnection = class extends EventTarget {
+	// onopen, ondata, onclose, onerror
+	#proto;
+	#host;
+	#port;
+	#source;
+	#sink;
+	#controller;
+	#reader; // DefaultReader
+	#writer; // DefaultWriter
+	#queue = []; // Data queue
+	#pool = []; // Stream queue
+	#readyState = 3;
+	#freed = false;
+	CONNECTING = 0;
+	OPEN = 1;
+	CLOSING = 2;
+	CLOSED = 3;
+	get protocol() {
+		return this.#proto;
+	};
+	get hostname() {
+		return this.#host;
+	};
+	get port() {
+		return this.#port;
+	};
+	get readyState() {
+		return this.#readyState;
+	};
+	get source() {
+		return this.#reader;
+	};
+	get sink() {
+		return this.#writer;
+	};
+	addEventListener(type, handler, opt) {
+		if (type == "open") {
+			if (this.readyState == this.OPEN) {
+				handler.call(this, new Event("open"));
+			};
+		};
+		super.addEventListener(type, handler, opt);
+	};
+	send(data) {
+		// Send data when connected
+		// Enqueued data otherwise
+		if (this.#freed) {
+			throw(new Error(`Cannot enqueue or send data on a freed connection`));
+		};
+		if (this.#readyState != 1) {
+			// Enqueue data in data queue
+			this.#queue.push(data);
+		} else if (this.#writer?.desiredSize < 0 || this.#pool.length) {
+			// Enqueue data in stream queue
+			this.#pool.push(data);
+			this.#writer.ready.then(() => {
+				let data = this.#pool.shift();
+				if (data) {
+					this.#writer.write(data);
+				};
+			});
+		} else {
+			// Send data
+			this.#writer.write(data);
+		};
+	};
+	async connect() {
+		// Start or restarts the connection
+		if (this.#freed) {
+			throw(new Error(`Cannot restart a freed connection`));
+		};
+		if (this.#readyState < this.CLOSING) {
+			console.debug(`${this.#proto.toUpperCase()} connection is already open.`);
+		};
+		// Set readyState to CONNECTING
+		this.#readyState = this.CONNECTING;
+		// Open the connection
+		// Set readyState to OPEN
+		this.#readyState = this.OPEN;
+	};
+	close() {
+		if (this.#readyState > this.OPEN) {
+			console.debug(`${this.#proto.toUpperCase()} connection is already closed.`);
+		};
+		// Set readyState to CLOSING
+		this.#readyState = this.CLOSING;
+		// Close the connection
+		// Set readyState to CLOSED
+		this.#readyState = this.CLOSED;
+	};
+	free() {
+		// Close and forbid any further connection attempts
+		this.close();
+		this.#freed = true;
+		// Returns any data not sent
+		return this.#queue.splice(0, this.#queue.length);
+	};
+	constructor({proto, host, port}, immediateConnect) {
+		super();
+		// Default values
+		proto = proto || "tcp";
+		host = host || "127.0.0.1";
+		port = port || 80;
+		// Send data upon connection
+		this.addEventListener("open", async () => {
+			this.#queue.forEach((e) => {
+				this.send(e);
+			});
+		});
+		// Move data back to queue if closed
+		this.addEventListener("close", () => {
+			if (this.#pool.length) {
+				this.#queue.splice(0, 0, this.#pool.splice(0, this.#pool.length));
+			};
+		});
+		// Create the underlying connection
+		// Connect immediately if set
+		if (immediateConnect) {
+			this.connect();
+		};
+	};
+};
+
 // Network interfaces
-let net = class {};
+let net = class {
+	static RawConnection = RawConnection;
+};
 
 export default net;
